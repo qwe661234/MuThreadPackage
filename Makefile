2a4146f3baa58abe4f892772b116b45867ebd853
Yen-Fu Chen <qwe661234@gmail.com>
2023-08-07 19:42:18 +0800

    Make all test files through make all

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
 CFLAGS = -Wall -fno-stack-protector -I. -pthread -lpthread
+CC := gcc
 
 ifeq ("$(PTHREAD)","1")
 	CFLAGS += -DPTHREAD
-	OBJ := Tests/$(FILE).c
 else
-	OBJ := Tests/$(FILE).c \
-		mutex.c \
+	OBJ := mutex.c \
 		condvar.c \
 		thread.c \
 		util.c \
@@ -13,16 +12,25 @@ else
 endif
 
 all:
-	gcc $(CFLAGS) -o test-$(NUM) $(OBJ)
+	$(CC) $(CFLAGS) -o test-01-basic Tests/test-01-basic.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-02-multiple-type Tests/test-02-multiple-type.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-03-add-count Tests/test-03-add-count.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-04-priority-inversion Tests/test-04-priority-inversion.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-05-priority-inversion-PI Tests/test-05-priority-inversion-PI.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-06-priority-inversion-PP Tests/test-06-priority-inversion-PP.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-07-benchmark Tests/test-07-benchmark.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-08-benchmark-PI Tests/test-08-benchmark-PI.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-09-benchmark-PP Tests/test-09-benchmark-PP.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-10-condvar-signal Tests/test-10-condvar-signal.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-11-condvar-signal_pi Tests/test-11-condvar-signal_pi.c $(OBJ)
+	$(CC) $(CFLAGS) -o test-12-prio-wake Tests/test-12-prio-wake.c $(OBJ)
 
 check:
-	make FILE=test-01-basic NUM=1
-	./test-1
+	$(CC) $(CFLAGS) -o test-01-basic Tests/test-01-basic.c $(OBJ)
+	./test-01-basic
 
 test:
 	Tests/driver.py
 
-cond:
-	make FILE=test-10-condvar-signal NUM=10
 clean: 
 	rm -f test-*
--- Tests/driver.py
+++ Tests/driver.py
@@ -13,21 +13,19 @@ TestFile = [
     "test-03-add-count", 
     "test-04-priority-inversion", 
     "test-05-priority-inversion-PI",   
-    "test-06-priority-inversion-PP",   
-    "test-07-PI-chaining-effect",
-    "test-08-benchmark",  
+    "test-06-priority-inversion-PP",  
+    "test-07-benchmark",  
 ]
 def printInColor(text, color):
     print(color, text, WHITE, sep = '')
 
 if __name__ == "__main__":
-    for i in range(8):
-        comp_proc = subprocess.run('make FILE={} NUM={} > /dev/null'.format(TestFile[i], i + 1), shell = True)
-        if comp_proc.returncode:
-            printInColor(" ERROR: Compile Fail \n", RED)
-            exit(1)
+    comp_proc = subprocess.run('make clean all> /dev/null', shell = True)
+    if comp_proc.returncode:
+        printInColor(" ERROR: Compile Fail \n", RED)
+        exit(1)
     for i in range(1,4):
-        comp_proc = subprocess.run('sudo ./test-{}'.format(i), shell = True)
+        comp_proc = subprocess.run('sudo ./{}'.format(TestFile[i - 1]), shell = True)
         if comp_proc.returncode:
             printInColor("+++ Test-{} Fail +++ \n".format(i), RED)
             exit(1)
@@ -36,7 +34,7 @@ if __name__ == "__main__":
     for i in range (4,7):
         PI_times = 0;
         for runs in range(3):
-            comp_proc = subprocess.run('sudo taskset -c 3 ./test-{} > data.txt'.format(i), shell = True)
+            comp_proc = subprocess.run('sudo taskset -c 3 ./{} > data.txt'.format(TestFile[i - 1]), shell = True)
             output = np.loadtxt('data.txt', dtype = 'int32').T      
             if (output[0] == 2 and output[1] == 3 and output[2] == 1):
                 PI_times += 1
@@ -51,11 +49,4 @@ if __name__ == "__main__":
                 printInColor("Priority Inheritance mutex, Priority_inversion times = {} for {} runs".format(PI_times, runs + 1), WHITE)
             if i == 6:
                 printInColor("Priority Protection mutex, Priority_inversion times = {} for {} runs".format(PI_times, runs + 1), WHITE)
-            printInColor("+++ Test-{} Pass +++ \n".format(i), GREEN)
-    for i in range(7,9):
-        comp_proc = subprocess.run('sudo ./test-{}'.format(i), shell = True)
-        if comp_proc.returncode:
-            printInColor("+++ Test-{} Fail +++ \n".format(i), RED)
-            exit(1)
-        else:
             printInColor("+++ Test-{} Pass +++ \n".format(i), GREEN)
